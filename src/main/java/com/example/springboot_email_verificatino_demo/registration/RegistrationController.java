4c3cd16cb31426f96322a43f941b2185e99c6ccd
Raj <rajmkumar369@gmail.com>
2023-06-22 07:53:12 +0530

    Registration controller updated

---
+++ src/main/java/com/example/springboot_email_verificatino_demo/registration/RegistrationController.java
@@ -0,0 +1,28 @@
+package com.example.springboot_email_verificatino_demo.registration;
+
+import com.example.springboot_email_verificatino_demo.user.User;
+import com.example.springboot_email_verificatino_demo.user.UserNameAlreadyExistsException;
+import com.example.springboot_email_verificatino_demo.user.UserRepository;
+import com.example.springboot_email_verificatino_demo.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/register")
+@RequiredArgsConstructor
+public class RegistrationController {
+
+    @Autowired
+    private final UserService userService;
+
+    public String registerUser(RegistrationRequest registrationRequest) throws UserNameAlreadyExistsException {
+        User user = userService.registerUser(registrationRequest);
+//        publish registration event
+        return "Success, Please Check you email to complete the registration";
+    }
+
+
+
+}
