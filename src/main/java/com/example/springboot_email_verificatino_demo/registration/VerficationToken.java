d5e0ddc7426d370ef0a21597e822a8e01b38f602
Raj <rajmkumar369@gmail.com>
2023-06-24 08:40:47 +0530

    VerificationToken class Update

--- src/main/java/com/example/springboot_email_verificatino_demo/registration/VerficationToken.java
+++ src/main/java/com/example/springboot_email_verificatino_demo/registration/VerficationToken.java
@@ -1,12 +1,41 @@
 package com.example.springboot_email_verificatino_demo.registration;
 
 import com.example.springboot_email_verificatino_demo.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.Calendar;
 import java.util.Date;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class VerficationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String token;
+
     private Date expirationTime;
+
+    private static final int EXPIRATION_TIME = 15;
+
+    @OneToOne
     private User user;
+
+    public VerficationToken(String token, User user) {
+        this.token = token;
+        this.user = user;
+        this.expirationTime = this.getTokenExpirationTime();
+    }
+
+    private Date getTokenExpirationTime() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(new Date().getTime());
+        calendar.add(Calendar.MINUTE, 10);
+    }
 }
